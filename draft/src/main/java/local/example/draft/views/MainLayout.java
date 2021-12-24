827807888b7f02f6231b38fc79e15cab28ec482d
paolomococci <paolomococci@gmail.com>
2021-12-24 05:38:38 +0100

    added MainLayout class belonging to the new demonstrative project draft

---
+++ draft/src/main/java/local/example/draft/views/MainLayout.java
@@ -0,0 +1,156 @@
+package local.example.draft.views;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.Nav;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.UnorderedList;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.auth.AccessAnnotationChecker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import local.example.draft.data.entity.User;
+import local.example.draft.security.AuthenticatedUser;
+import local.example.draft.views.hello.HelloView;
+import local.example.draft.views.home.HomeView;
+
+@PageTitle("Main")
+public class MainLayout
+        extends AppLayout {
+
+    public static class MenuItemInfo {
+
+        private String text;
+        private String iconClass;
+        private Class<? extends Component> view;
+
+        public MenuItemInfo(String text, String iconClass, Class<? extends Component> view) {
+            this.text = text;
+            this.iconClass = iconClass;
+            this.view = view;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String getIconClass() {
+            return iconClass;
+        }
+
+        public Class<? extends Component> getView() {
+            return view;
+        }
+
+    }
+
+    private AuthenticatedUser authenticatedUser;
+    private AccessAnnotationChecker accessChecker;
+
+    public MainLayout(AuthenticatedUser authenticatedUser, AccessAnnotationChecker accessChecker) {
+        this.authenticatedUser = authenticatedUser;
+        this.accessChecker = accessChecker;
+
+        addToNavbar(createHeaderContent());
+    }
+
+    private Component createHeaderContent() {
+        Header header = new Header();
+        header.addClassNames("bg-base", "border-b", "border-contrast-10", "box-border", "flex", "flex-col", "w-full");
+
+        Div layout = new Div();
+        layout.addClassNames("flex", "h-xl", "items-center", "px-l");
+
+        H1 appName = new H1("Draft");
+        appName.addClassNames("my-0", "me-auto", "text-l");
+        layout.add(appName);
+
+        Optional<User> maybeUser = authenticatedUser.get();
+        if (maybeUser.isPresent()) {
+            User user = maybeUser.get();
+
+            Avatar avatar = new Avatar(user.getName(), user.getProfilePictureUrl());
+            avatar.addClassNames("me-xs");
+
+            ContextMenu userMenu = new ContextMenu(avatar);
+            userMenu.setOpenOnClick(true);
+            userMenu.addItem("Logout", e -> {
+                authenticatedUser.logout();
+            });
+
+            Span name = new Span(user.getName());
+            name.addClassNames("font-medium", "text-s", "text-secondary");
+
+            layout.add(avatar, name);
+        } else {
+            Anchor loginLink = new Anchor("login", "Sign in");
+            layout.add(loginLink);
+        }
+
+        Nav nav = new Nav();
+        nav.addClassNames("flex", "gap-s", "overflow-auto", "px-m");
+
+        UnorderedList list = new UnorderedList();
+        list.addClassNames("flex", "list-none", "m-0", "p-0");
+        nav.add(list);
+
+        for (RouterLink link : createLinks()) {
+            ListItem item = new ListItem(link);
+            list.add(item);
+        }
+
+        header.add(layout, nav);
+        return header;
+    }
+
+    private List<RouterLink> createLinks() {
+        MenuItemInfo[] menuItems = new MenuItemInfo[]{
+                new MenuItemInfo(
+                        "Home",
+                        "la la-home",
+                        HomeView.class
+                ),
+                new MenuItemInfo(
+                        "Hello",
+                        "la la-globe",
+                        HelloView.class
+                )
+        };
+        List<RouterLink> links = new ArrayList<>();
+        for (MenuItemInfo menuItemInfo : menuItems) {
+            if (accessChecker.hasAccess(menuItemInfo.getView())) {
+                links.add(createLink(menuItemInfo));
+            }
+        }
+        return links;
+    }
+
+    private static RouterLink createLink(MenuItemInfo menuItemInfo) {
+        RouterLink link = new RouterLink();
+        link.addClassNames("flex", "h-m", "items-center", "px-s", "relative", "text-secondary");
+        link.setRoute(menuItemInfo.getView());
+
+        Span icon = new Span();
+        icon.addClassNames("me-s", "text-l");
+        if (!menuItemInfo.getIconClass().isEmpty()) {
+            icon.addClassNames(menuItemInfo.getIconClass());
+        }
+
+        Span text = new Span(menuItemInfo.getText());
+        text.addClassNames("font-medium", "text-s", "whitespace-nowrap");
+
+        link.add(icon, text);
+        return link;
+    }
+}
