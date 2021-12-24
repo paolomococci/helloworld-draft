42fa5018cd54e4a87e8d6889057ee5883743b949
paolomococci <paolomococci@gmail.com>
2021-12-24 05:42:27 +0100

    added UserService class belonging to the new demonstrative project draft

---
+++ draft/src/main/java/local/example/draft/data/service/UserService.java
@@ -0,0 +1,42 @@
+package local.example.draft.data.service;
+
+import java.util.Optional;
+
+import local.example.draft.data.entity.User;
+import local.example.draft.data.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository repository;
+
+    public UserService(@Autowired UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public Optional<User> get(Integer id) {
+        return repository.findById(id);
+    }
+
+    public User update(User entity) {
+        return repository.save(entity);
+    }
+
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+
+    public Page<User> list(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    public int count() {
+        return (int) repository.count();
+    }
+
+}
