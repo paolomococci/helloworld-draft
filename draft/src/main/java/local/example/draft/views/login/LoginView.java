aba59edbed4b8d1ea94e3402f9476cfb265bdc9d
paolomococci <paolomococci@gmail.com>
2021-12-24 05:38:01 +0100

    added LoginView class belonging to the new demonstrative project draft

---
+++ draft/src/main/java/local/example/draft/views/login/LoginView.java
@@ -0,0 +1,26 @@
+package local.example.draft.views.login;
+
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Login")
+@Route(value = "login")
+public class LoginView
+        extends LoginOverlay {
+
+    public LoginView() {
+        setAction("login");
+
+        LoginI18n i18n = LoginI18n.createDefault();
+        i18n.setHeader(new LoginI18n.Header());
+        i18n.getHeader().setTitle("Draft");
+        i18n.getHeader().setDescription("Login");
+        i18n.setAdditionalInformation(null);
+        setI18n(i18n);
+
+        setForgotPasswordButtonVisible(false);
+        setOpened(true);
+    }
+}
