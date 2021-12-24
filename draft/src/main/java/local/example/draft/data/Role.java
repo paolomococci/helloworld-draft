6ae41ee69285ddb10ba8aabf342ef88a7f780b2b
paolomococci <paolomococci@gmail.com>
2021-12-24 05:39:05 +0100

    added Role enum belonging to the new demonstrative project draft

---
+++ draft/src/main/java/local/example/draft/data/Role.java
@@ -0,0 +1,17 @@
+package local.example.draft.data;
+
+public enum Role {
+
+    USER("user"),
+    ADMIN("admin");
+
+    private final String roleName;
+
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
