cb9c687b9118c6fd5fcd95ece554489715fd8f7e
paolomococci <paolomococci@gmail.com>
2022-09-03 17:09:02 +0200

    I edited OutcomeResource.java belonging to the hello-grpc project

--- hello-grpc/src/main/java/local/example/outcome/OutcomeResource.java
+++ hello-grpc/src/main/java/local/example/outcome/OutcomeResource.java
@@ -1,5 +1,10 @@
 package local.example.outcome;
 
+import io.quarkus.grpc.GrpcClient;
+import io.smallrye.mutiny.Uni;
+import local.example.hello.grpc.Greeter;
+import local.example.hello.grpc.HelloRequest;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +13,22 @@ import javax.ws.rs.core.MediaType;
 @Path("/outcome")
 public class OutcomeResource {
 
+    @GrpcClient
+    Greeter greeter;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello";
+        return "Hello\n";
+    }
+
+    @GET
+    @Path("/{name}")
+    public Uni<String> hello(String name) {
+        return greeter.sayHello(
+                HelloRequest.newBuilder().setName(name).build()
+        ).onItem().transform(
+                helloReply -> helloReply.getMessage()
+        );
     }
 }
