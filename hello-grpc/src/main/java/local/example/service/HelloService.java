240ae99395745298c10a830fb817558823a03428
paolomococci <paolomococci@gmail.com>
2022-09-03 17:08:40 +0200

    I edited HelloService.java belonging to the hello-grpc project

--- hello-grpc/src/main/java/local/example/service/HelloService.java
+++ hello-grpc/src/main/java/local/example/service/HelloService.java
@@ -1,7 +1,21 @@
 package local.example.service;
 
 import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+
+import local.example.hello.grpc.Greeter;
+import local.example.hello.grpc.HelloReply;
+import local.example.hello.grpc.HelloRequest;
 
 @GrpcService
-public class HelloService {
+public class HelloService implements Greeter {
+
+    @Override
+    public Uni<HelloReply> sayHello(HelloRequest helloRequest) {
+        return Uni.createFrom().item(
+                () -> HelloReply.newBuilder().setMessage(
+                        "Hello " + helloRequest.getName()
+                ).build()
+        );
+    }
 }
