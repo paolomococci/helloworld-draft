e592352fa24db75c854935f8821ac154318befc4
paolomococci <paolomococci@gmail.com>
2022-09-03 10:54:14 +0200

    I edited class HelloReply belonging to project hello-service

--- hello-service/src/main/kotlin/local/example/gprc/HelloReply.kt
+++ hello-service/src/main/kotlin/local/example/gprc/HelloReply.kt
@@ -1,4 +1,12 @@
 package local.example.gprc
 
+import local.example.grpc.HelloReply
+
 class HelloReply {
+    companion object {
+        @JvmStatic
+        fun newBuilder(): HelloReply.Builder {
+            TODO("Not yet implemented")
+        }
+    }
 }
