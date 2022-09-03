3cff05aca2dc811d2ee382cce3195f0746fdc1e2
paolomococci <paolomococci@gmail.com>
2022-09-03 10:55:08 +0200

    I edited class HelloService belonging to project hello-service

--- hello-service/src/main/kotlin/local/example/service/HelloService.kt
+++ hello-service/src/main/kotlin/local/example/service/HelloService.kt
@@ -1,7 +1,17 @@
 package local.example.service
 
 import io.quarkus.grpc.GrpcService
+import io.smallrye.mutiny.Uni
+
+import local.example.gprc.Greeter
+import local.example.gprc.HelloRequest
+import local.example.grpc.HelloReply
 
 @GrpcService
-class HelloService {
+class HelloService : Greeter {
+    override fun sayHello(request: HelloRequest): Uni<HelloReply> {
+        return Uni.createFrom().item {
+            HelloReply.newBuilder().setMessage("Hello " + request.name).build()
+        }
+    }
 }
