e1fba930a423178528c2f67d93394637ae973f84
paolomococci <paolomococci@gmail.com>
2022-09-03 15:18:01 +0200

    I edited OutcomeResourceIT.java belonging to the hello-grpc project

--- hello-grpc/src/test/java/local/example/outcome/OutcomeResourceIT.java
+++ hello-grpc/src/test/java/local/example/outcome/OutcomeResourceIT.java
@@ -3,6 +3,6 @@ package local.example.outcome;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class OutcomeResourceIT extends OutcomeResourceTest {
-    // Execute the same tests but in packaged mode.
+public class OutcomeResourceIT
+        extends OutcomeResourceTest {
 }
